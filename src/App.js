afb5de732d57b0cf05d9d230b790f4940039b01c
gabriel velez <gabriel.dan.velez@gmail.com>
2022-04-28 00:06:57 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,61 +1,62 @@
 import React, { useEffect, useState } from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
 
-import MovieHeader from './components/MovieHeader';
+import MovieHeader from "./components/MovieHeader";
 
-import FavoriteMovieList from './components/FavoriteMovieList';
+import EditMovieForm from "./components/EditMovieForm";
 
-import axios from 'axios';
+import FavoriteMovieList from "./components/FavoriteMovieList";
+
+import axios from "axios";
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
 
-  useEffect(()=>{
-    axios.get('http://localhost:9000/api/movies')
-      .then(res => {
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/api/movies")
+      .then((res) => {
         setMovies(res.data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  const deleteMovie = (id)=> {
-  }
+  const deleteMovie = (id) => {};
 
-  const addToFavorites = (movie) => {
-    
-  }
+  const addToFavorites = (movie) => {};
 
   return (
     <div>
-      <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" > HTTP / CRUD Module Project</span>
+      <nav className='navbar navbar-dark bg-dark'>
+        <span className='navbar-brand'> HTTP / CRUD Module Project</span>
       </nav>
 
-      <div className="container">
-        <MovieHeader/>
-        <div className="row ">
-          <FavoriteMovieList favoriteMovies={favoriteMovies}/>
-        
+      <div className='container'>
+        <MovieHeader />
+        <div className='row '>
+          <FavoriteMovieList favoriteMovies={favoriteMovies} />
+
           <Switch>
-            <Route path="/movies/edit/:id">
+            <Route path='/movies/edit/:id'>
+              <EditMovieForm setMovies={setMovies} />
             </Route>
 
-            <Route path="/movies/:id">
-              <Movie/>
+            <Route path='/movies/:id'>
+              <Movie setMovies={setMovies} />
             </Route>
 
-            <Route path="/movies">
-              <MovieList movies={movies}/>
+            <Route path='/movies'>
+              <MovieList movies={movies} />
             </Route>
 
-            <Route path="/">
-              <Redirect to="/movies"/>
+            <Route path='/'>
+              <Redirect to='/movies' />
             </Route>
           </Switch>
         </div>
@@ -64,6 +65,4 @@ const App = (props) => {
   );
 };
 
-
 export default App;
-
